ab07bcf230f4b85da0891ecb2b53bff4ad471567
Docta Rhyf <drrhyf@gmail.com>
2024-08-01 20:10:59 +0200

    added navigation

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,22 +1,36 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
+
+const LINKS = [
+  { to: "/", title: "Home" },
+  { to: "/about", title: "About" },
+  { to: "/support", title: "Support" },
+  { space: true },
+  { to: "/logout", title: "Logout" },
+];
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/support">Support</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link>
-          </li>
+        <ul className=" flex flex-col  md:flex-row  bg-sky-800  ">
+          {LINKS.map((lk, i) =>
+            lk.space ? (
+              <li key={i} className=" flex-grow "></li>
+            ) : (
+              <li key={i}>
+                <Link
+                  className={` w-full md:w-fit  ${
+                    location.pathname === lk.to ? "bg-sky-500" : ""
+                  }   p-2 inline-block hover:bg-white/20   `}
+                  to={lk.to as string}
+                >
+                  {lk.title}
+                </Link>
+              </li>
+            )
+          )}
         </ul>
       </nav>
 
--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,15 +1,20 @@
 import { useContext } from "react";
 import { UserContext } from "../App";
+import { useNavigate } from "react-router-dom";
 
 export default function Logout() {
   const [_, setuser] = useContext(UserContext);
+  const navigate = useNavigate();
 
   return (
     <div>
       <div>Confirm logout?</div>
       <button
         className=" bg-red-950 rounded-md text-white p-1 text-xs   "
-        onClick={(_) => setuser(undefined)}
+        onClick={(_) => {
+          setuser(undefined);
+          navigate("/");
+        }}
       >
         LOGOUT
       </button>
