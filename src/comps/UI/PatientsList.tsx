96144d310a7d8a7bdc1f9b5299eee4c479d03e01
doctarhyf <drrhyf@gmail.com>
2024-08-02 22:13:09 +0200

    upd

--- src/comps/UI/PatientsList.tsx
+++ src/comps/UI/PatientsList.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { GetDepartementLabel } from "../../helpers/funcs";
-import { TPatient } from "../../helpers/types";
+import { IDepartment, TPatient } from "../../helpers/types";
+import { DEPARTEMENTS } from "../../helpers/const";
 
 export default function PatientsList({
   patientsf,
@@ -10,28 +12,61 @@ export default function PatientsList({
   onPatientSelected: (pat: TPatient) => void;
   selectedPatient: TPatient | undefined;
 }) {
+  const [dep, setdep] = useState<string>(DEPARTEMENTS.SOINS_CURRATIFS.code);
+
   return (
     <div className={`  ${selectedPatient ? "hidden" : "block"} w-full sm:w-52`}>
-      {patientsf?.map((pat, i) => (
-        <div
-          key={i}
-          onClick={(_) => onPatientSelected(pat)}
-          className={` ${
-            pat.id === selectedPatient?.id && "bg-sky-800"
-          } w-full  cursor-pointer hover:bg-sky-500 hover:text-white 
-             border p-2 mb-4 border-slate-500 hover:border-sky-500`}
-        >
-          <div className={` ${pat.left_at && " line-through italic "}  `}>
-            {pat.prenom}, {pat.nom} {pat.postnom}{" "}
-          </div>
-          <div className="flex  gap-2 items-center font-bold text-sky-400 text-xs   ">
-            <span className="flex-grow"> {GetDepartementLabel(pat.dep)} </span>
-            {pat.left_at && (
-              <span className="bg-red-500 text-white w-4 h-4 rounded-full  text-[8pt] font-bold   "></span>
-            )}
-          </div>
+      <div className="my-2">
+        <div>
+          Filter by Departement (
+          {
+            patientsf?.filter((it) => (dep === "all" ? true : it.dep === dep))
+              .length
+          }
+          )
         </div>
-      ))}
+        <select
+          value={dep}
+          onChange={(e) => setdep(e.target.value)}
+          className=" outline-none p-1 border border-sky-500 hover:border-sky-400 focus:border-purple-500  "
+        >
+          {Object.entries(DEPARTEMENTS).map(
+            (dep: [key: string, d: IDepartment], i: number) => (
+              <option key={i} value={dep[1].code}>
+                {dep[1].label}
+              </option>
+            )
+          )}
+          <option value={"all"}>ALL</option>
+        </select>
+      </div>
+      <div>
+        {patientsf
+          ?.filter((it) => (dep === "all" ? true : it.dep === dep))
+          .map((pat, i) => (
+            <div
+              key={i}
+              onClick={(_) => onPatientSelected(pat)}
+              className={` ${
+                pat.id === selectedPatient?.id && "bg-sky-800"
+              } w-full  cursor-pointer hover:bg-sky-500 hover:text-white 
+             border p-2 mb-4 border-slate-500 hover:border-sky-500`}
+            >
+              <div className={` ${pat.left_at && " line-through italic "}  `}>
+                {pat.prenom}, {pat.nom} {pat.postnom}{" "}
+              </div>
+              <div className="flex  gap-2 items-center font-bold text-sky-400 text-xs   ">
+                <span className="flex-grow">
+                  {" "}
+                  {GetDepartementLabel(pat.dep)}{" "}
+                </span>
+                {pat.left_at && (
+                  <span className="bg-red-500 text-white w-4 h-4 rounded-full  text-[8pt] font-bold   "></span>
+                )}
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
