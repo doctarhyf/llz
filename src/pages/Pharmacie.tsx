9299977759d5ce8e8d50aa94852221dbbdfb21a5
doctarhyf <drrhyf@gmail.com>
2024-08-04 07:37:21 +0200

    dbg

--- src/pages/Pharmacie.tsx
+++ src/pages/Pharmacie.tsx
@@ -1,8 +1,4 @@
-import { useEffect, useState } from "react";
 import MedsList from "../comps/UI/MedsList";
-import * as SB from "../db/sb";
-import { TABLES_NAMES } from "../helpers/sb.config";
-import { TMed } from "../helpers/types";
 
 export default function Pharmacie() {
   return (
